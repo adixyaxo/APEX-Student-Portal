0e18b95c543dcb7af0eda61eb8493717b9c04598
adixyaxo <aditya0dagar@gmail.com>
2026-05-31 11:24:14 +0530

    learning the working

--- server.cpp
+++ server.cpp
@@ -14,5 +14,17 @@ int main() {
         res.set_content("Routes route","text/plain");
     });
 
+    svr.Get(R"(/files/(\d+)/(\d+))", [](const auto &req, auto &res) {
+        auto id = req.matches[1];
+        auto id_2 = req.matches[2];
+        res.set_content("File ID: " + std::string(id)+ std::string(id_2), "text/plain");
+    });
+
+     svr.Get("/users/:id", [](const auto &req, auto &res) {
+        auto id = req.path_params.at("id");
+        res.set_content("User ID: " + id, "text/plain");
+    });
+
+
     svr.listen("0.0.0.0", 8080);
 }
